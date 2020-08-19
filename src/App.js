787ee35958b00bf88b4229e0ec3dd7a5816aae7f
hardiknai <hardiknai92@gmail.com>
2020-08-19 17:04:33 +0530

    First time code commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Carousel } from "react-bootstrap";
+import one from "./img/01.jpg";
+import two from "./img/02.jpg";
+import three from "./img/03.jpg";
+import four from "./img/04.jpg";
 
+const initialState = {
+  images: [
+    {
+      src: one,
+      color: "white",
+    },
+    {
+      src: two,
+      color: "white",
+    },
+    {
+      src: three,
+      color: "Green",
+    },
+    {
+      src: four,
+      color: "Blue",
+    },
+  ],
+};
 function App() {
+  const [state, setstate] = useState(initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel>
+      {state.images &&
+        state.images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100" src={image.src} alt="First slide" height="200px"/>
+            <Carousel.Caption style={{color:image.color}}>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+    </Carousel>
   );
 }
 
